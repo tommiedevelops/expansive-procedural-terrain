d74a74936f10ebb454b4331f944ab312a0820951
Thomas Dizon <thomas.dizon25@gmail.com>
2025-02-22 12:57:42 +1100

    Added simple perlin noise 2D texture generator. Demo: https://drive.google.com/file/d/1pC3TGmpzoVOfSxJRwoZtSRx4He_m0rL8/view?usp=drive_link

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+    public override void OnInspectorGUI() {
+        MapGenerator mapGen = target as MapGenerator; // object that this custom editor is inspecting
+        
+        if (DrawDefaultInspector() && mapGen.autoUpdate) {
+            mapGen.GenerateMap();
+        }
+
+        if (GUILayout.Button("Generate")) {
+            mapGen.GenerateMap();
+        }
+    }
+
+}
---
+++ Assets/MapDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MapDisplay : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] Renderer textureRenderer;
+
+    public void DrawNoiseMap(float[,] noiseMap) {
+        int width = noiseMap.GetLength(0); 
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D (width, height);
+        Color[] colorMap = new Color[width * height]; //1D array
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // convert 2D array into 1D representation (think it through)
+                // what is this line of code doing? 
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+        }
+
+        texture.SetPixels(colorMap); // faster than SetPixel()
+        texture.Apply();
+
+        //textureRenderer.material is only instantiated at runtime
+        textureRenderer.sharedMaterial.mainTexture = texture; // So we can see it w/o having to run game
+        textureRenderer.transform.localScale = new Vector3(width, 1f, height);
+    }
+
+}
