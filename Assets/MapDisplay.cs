13c821592f9200a0e4c703e39a52d3addf0f9505
Thomas Dizon <thomas.dizon25@gmail.com>
2025-02-23 10:16:05 +1100

    Added colour map

--- Assets/MapDisplay.cs
+++ Assets/MapDisplay.cs
@@ -4,25 +4,40 @@ public class MapDisplay : MonoBehaviour {
     [Header("References")]
     [SerializeField] Renderer textureRenderer;
 
-    public void DrawNoiseMap(float[,] noiseMap) {
-        int width = noiseMap.GetLength(0); 
-        int height = noiseMap.GetLength(1);
+    public void DrawTexture(Texture2D texture) {
 
-        var texture = new Texture2D(width, height);
+        textureRenderer.sharedMaterial.mainTexture = texture; // So we can see it w/o having to run game
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1f, texture.height);
+    }
+    
+
+}
+
+public static class TextureGenerator {
+    public static int Convert2DIndexTo1D(int x, int y, int width) => y * width + x;
+    public static Texture2D GetTextureFromColorMap(Color[] colorMap, int mapWidth, int mapHeight) {
+        Texture2D texture = new Texture2D(mapWidth, mapHeight);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+
+        texture.SetPixels(colorMap);
+        texture.Apply();
+        return texture;
+    }
+
+    public static Texture2D GetTextureFromHeightMap(float[,] heightMap) {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+
         Color[] colorMap = new Color[width * height]; //1D array
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 // convert 2D array into 1D representation (think it through)
-                colorMap[Convert2DIndexTo1D(x,y,width)] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+                colorMap[Convert2DIndexTo1D(x, y, width)] = Color.Lerp(Color.black, Color.white, heightMap[x, y]);
             }
         }
 
-        texture.SetPixels(colorMap); // faster than SetPixel()
-        texture.Apply();
-        textureRenderer.sharedMaterial.mainTexture = texture; // So we can see it w/o having to run game
-        textureRenderer.transform.localScale = new Vector3(width, 1f, height);
+        return GetTextureFromColorMap(colorMap, width, height);
     }
-    int Convert2DIndexTo1D(int x, int y, int width) => y * width + x; 
-
 }
--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,10 +1,13 @@
+using NUnit.Framework;
+using System;
 using UnityEngine;
-
+using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour {
     #region Fields&Consts
     [Header("Values")]
     [SerializeField] GenerateNoiseMapParams parameters;
+    [SerializeField] List<TerrainType> regions;
     
     public bool autoUpdate;
 
@@ -14,16 +17,45 @@ public class MapGenerator : MonoBehaviour {
 
     
     #endregion
+    
+    public enum DrawMode {
+        NoiseMap,
+        ColorMap
+    }
+
+    public DrawMode drawMode;
     public void GenerateMap() {
         float[,] noiseMap = Noise.GenerateNoiseMap(parameters);
         MapDisplay display = GetComponent<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        Color[] colorMap = GenerateColorMap(noiseMap);
+
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.GetTextureFromHeightMap(noiseMap));
+        } else if(drawMode == DrawMode.ColorMap) {
+            display.DrawTexture(TextureGenerator.GetTextureFromColorMap(colorMap, parameters.mapWidth, parameters.mapHeight));
+        }
+
+    }
+
+    private Color[] GenerateColorMap(float[,] noiseMap) {
+        Color[] colorMap = new Color[parameters.mapWidth * parameters.mapHeight];
+        for (int y = 0; y < parameters.mapHeight; y++) {
+            for (int x = 0; x < parameters.mapWidth; x++) {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Count; i++) {
+                    if (currentHeight <= regions[i].height) {
+                        colorMap[y * parameters.mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+        return colorMap;
     }
 
     void OnValidate() {
         ValidateFields();
     }
-
     void ValidateFields() {
         if (parameters.mapWidth < 1) parameters.mapWidth = ONE;
         if (parameters.mapHeight < 1) parameters.mapHeight = ONE;
@@ -34,4 +66,10 @@ public class MapGenerator : MonoBehaviour {
         if (parameters.octaves <= 0) parameters.octaves = ONE;
     }
 
+    [Serializable]
+    public struct TerrainType {
+        public string name;
+        public float height;
+        public Color color;
+    }
 }
