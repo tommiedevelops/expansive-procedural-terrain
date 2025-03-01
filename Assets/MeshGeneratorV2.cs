bc9d9afaafdf214bd9a73cbfc1979d77c7652a46
Thomas Dizon <tdiz6553@uni.sydney.edu.au>
2025-03-01 18:48:41 +1100

    Added first attempt at triangle generation code

--- Assets/MeshGeneratorV2.cs
+++ Assets/MeshGeneratorV2.cs
@@ -8,10 +8,8 @@ public class MeshGeneratorV2 : MonoBehaviour {
 
     MeshData meshData;
     Mesh mesh;
-    private int planeWidth = 10;
-    private int planeHeight = 10;
-
-    public void ClearMesh() {
+    [SerializeField] int planeWidth = 10;
+    [SerializeField] int planeLength = 10;
 
     public struct MeshData {
         public readonly Vector3[] vertices;
@@ -30,9 +28,9 @@ public class MeshGeneratorV2 : MonoBehaviour {
             Gizmos.DrawSphere(meshData.vertices[i], 0.1f);
     }
 
-    public void DrawMesh() {
+    public void DrawPlaneMesh() {
         MeshFilter meshFilter = GetComponent<MeshFilter>();
-        MeshData planeMeshData = CreatePlaneMeshData(planeWidth, planeHeight);
+        MeshData planeMeshData = CreatePlaneMeshData(planeWidth, planeLength);
         meshData = planeMeshData;
 
         mesh = new() {
@@ -55,17 +53,41 @@ public class MeshGeneratorV2 : MonoBehaviour {
          */
 
         int numVertices = width * length;
-        int numTriangles = 3*4 * numVertices / 2;
+        int numTriangleVertices = 6 * numVertices;
 
         Vector3[] vertices = new Vector3[numVertices];
-        int[] triangles = new int[numTriangles];
+        int[] triangles = new int[numTriangleVertices];
 
+        // Create vertices
         for(int i = 0, y = 0; y < length; y++) {
             for(int x=0; x < width; x++) {
                 vertices[i] = new Vector3(x, 0f, y);
                 i++;
             }
         }
+
+        // Create triangles
+        // suppose we have vertices
+        // a b e f
+        // c d g h
+        // a is responsible for triangles (a,d,c) and (a,b,d)
+        // e is responsible for triangles (e,h,g) and (e,f,h)
+        // triangles = {}
+        for(int i = 0, y = 0; y < 5; y++) {
+
+            for(int x = 0; x < 5; x++) {
+                // first triangle
+                triangles[i] = i;
+                triangles[i + 1] = i + width + 1;
+                triangles[i+2] = i + width;
+                // second triangle
+                triangles[i+3] = i;
+                triangles[i+4] = i + 1;
+                triangles[i+5] = i + width + 1;
+                i+=6;
+            }
+        }
+
 
         return new MeshData(vertices, triangles);
 
@@ -78,16 +100,18 @@ public class MeshGeneratorV2 : MonoBehaviour {
 public class MeshGeneratorEditor : Editor {
 
     MeshGeneratorV2 meshGen;
+
     public override void OnInspectorGUI() {
         MeshGeneratorV2 meshGen = (MeshGeneratorV2)target;
 
         if (GUILayout.Button("Generate")) {
-            meshGen.DrawMesh();
+            meshGen.DrawPlaneMesh();
         }
 
-        if (GUILayout.Button("Clear") {
-            meshGen.
+        if(GUILayout.Button("Clear Gizmos")) {
+            SceneView.RepaintAll();
         }
+
     }
 
 
