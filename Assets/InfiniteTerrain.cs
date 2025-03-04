9b000864194bf95f2bd2e902656d45b2f6adbd59
Thomas Dizon <thomas.dizon25@gmail.com>
2025-03-05 10:28:23 +1100

    Implmeneted dynamic chunk generation and disabling. Next step is probably level of detail. Demo: https://drive.google.com/file/d/1xHOK1GiipkD_85UPE2Dy_1u5niNf5QPG/view?usp=drive_link

--- Assets/InfiniteTerrain.cs
+++ Assets/InfiniteTerrain.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using System;
-using UnityEditor.Rendering;
-using System.Runtime.CompilerServices;
+using System.Drawing;
+using UnityEngine.UIElements;
 
 public class InfiniteTerrain : MonoBehaviour {
-    private const int CHUNK_SIZE = 240;
+
+    public static InfiniteTerrain Instance { get; private set; }
+
+    public const int CHUNK_SIZE = 240;
     [SerializeField] Transform viewer;
     [SerializeField] float renderDistance = CHUNK_SIZE;
+    [SerializeField] public Material redMaterial;
+
     int chunksRenderDistance;
     Vector2 viewerChunkCoords;
 
     private Dictionary<Vector2, TerrainChunkV2> terrainChunks = new();
 
+    private void Awake() {
+        if (Instance == null) Instance = this;
+    }
     private void Start() {
         UpdateVariables();
     }
@@ -25,7 +32,7 @@ public class InfiniteTerrain : MonoBehaviour {
                 var specificChunkCoords = new Vector2(viewerChunkCoords.x + xOffset, viewerChunkCoords.y + yOffset);
 
                 if (terrainChunks.ContainsKey(specificChunkCoords)) {
-                    // do something nice
+                    terrainChunks[specificChunkCoords].UpdateTerrainChunk();
                 } else {
                     terrainChunks.Add(specificChunkCoords, new TerrainChunkV2(specificChunkCoords));
                 }
@@ -38,12 +45,18 @@ public class InfiniteTerrain : MonoBehaviour {
         chunksRenderDistance = Mathf.RoundToInt(renderDistance / CHUNK_SIZE);
     }
 
+    // Getter
+    public Vector2 GetViewerPosition() { return new Vector2(viewer.position.x, viewer.position.z); }
+    public float GetRenderDistance() { return renderDistance; }
+
 }
 
 public class TerrainChunkV2 {
     private const float DEFAULT_SCALE = 1.0f;
-    private const int CHUNK_SIZE = 240;
+    private const int CHUNK_SIZE = InfiniteTerrain.CHUNK_SIZE;
     private readonly GameObject chunkObject;
+
+    Bounds bounds;
 
     public TerrainChunkV2(Vector2 viewerChunkCoords) {
         Mesh newMesh = PlaneMeshGenerator.GeneratePlaneMesh(CHUNK_SIZE, CHUNK_SIZE, DEFAULT_SCALE);
@@ -52,6 +65,17 @@ public class TerrainChunkV2 {
         // use default material for now
         chunkObject.GetComponent<MeshRenderer>().material = UnityEngine.Rendering.GraphicsSettings.defaultRenderPipeline.defaultMaterial;
         chunkObject.transform.position = new Vector3(viewerChunkCoords.x, 0f, viewerChunkCoords.y) * CHUNK_SIZE;
+        var viewerChunkCoordsCenter = new Vector2(viewerChunkCoords.x + 0.5f, viewerChunkCoords.y + 0.5f);
+        bounds = new Bounds(viewerChunkCoordsCenter * CHUNK_SIZE, Vector2.one * (float)CHUNK_SIZE/2);
     }
 
+    public void UpdateTerrainChunk() {
+        float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(InfiniteTerrain.Instance.GetViewerPosition()));
+        bool isVisible = viewerDstFromNearestEdge <= InfiniteTerrain.Instance.GetRenderDistance();
+        SetVisible(isVisible);
+    }
+
+    private void SetVisible(bool isVisible) {
+        chunkObject.SetActive(isVisible);
+    }
 }
