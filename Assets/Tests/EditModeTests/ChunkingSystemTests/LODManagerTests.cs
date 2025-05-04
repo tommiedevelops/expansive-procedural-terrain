3c270cdfcb5d0beea6cd20ed818889609e895f87
Thomas Dizon <thomas.dizon25@gmail.com>
2025-05-04 16:43:38 +1000

    Added more LODManager tests. Getting into the habit of always finishing on a failing test case

--- Assets/Tests/EditModeTests/ChunkingSystemTests/LODManagerTests.cs
+++ Assets/Tests/EditModeTests/ChunkingSystemTests/LODManagerTests.cs
@@ -3,7 +3,7 @@ using _Scripts.ChunkingSystem;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-
+// ReSharper disable all
 namespace EditModeTests
 {
     public class LODManagerTests
@@ -36,15 +36,53 @@ namespace EditModeTests
         }
         
         [Test]
-        public void Can_Generate_LODs_From_Min_Chunk_Size()
+        public void Can_Generate_Correct_LODs_From_Min_Chunk_Size()
         {
-            int testMinChunkSize = 12;
+            var lodManagerUnderTest = new LODManager();
+            const int testMinChunkSize = 12;
+            lodManagerUnderTest.SetMinChunkSize(testMinChunkSize);
             var expectedLods = new int[] { 1, 2, 3, 4, 6, 12 };
-            var lods = LODManager.ComputeLODs(testMinChunkSize);
+            var lods = lodManagerUnderTest.ComputeLODsFromMinChunkSize();
             
             Assert.That(lods, Is.EquivalentTo(expectedLods));
         }
         
+        [Test]
+        public void Can_Generate_Correct_LODs_From_Larger_Chunk_Size()
+        {
+            var lodManagerUnderTest = new LODManager();
+            const int chunkSize = 240;
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 20, 24, 30, 40, 48, 60, 80, 120, 240 };
+            lodManagerUnderTest.SetMinChunkSize(chunkSize);
+            var  lods = lodManagerUnderTest.ComputeLODsFromMinChunkSize();
+            Assert.That(lods, Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Stores_A_Max_LOD_Level()
+        {
+            var lodManagerUnderTest = new LODManager();
+            int maxLODLevel = 5;
+            lodManagerUnderTest.SetMaxLODLevel(maxLODLevel);
+            int level = lodManagerUnderTest.GetMaxLODLevel();
+            Assert.That(level, Is.EqualTo(maxLODLevel));
+        }
+
+        [TestCase(0,5,12)]
+        [TestCase(5,5,1)]
+        [TestCase(5, 4, 2)]
+        [TestCase(7, 10,1)]
+        public void Calculates_Correct_LOD(int level, int maxLODLevel, int expected)
+        {
+            var lodManagerUnderTest = new LODManager();
+            lodManagerUnderTest.SetMinChunkSize(12); // {1,2,3,4,6,12}
+            lodManagerUnderTest.SetMaxLODLevel(maxLODLevel);
+            
+            var lods = lodManagerUnderTest.ComputeLODsFromMinChunkSize();
+            
+            Assert.That(lodManagerUnderTest.ComputeLOD(level), Is.EqualTo(expected));
+            
+        }
     }
     
 }
--- Assets/_Scripts/ChunkingSystem/LODManager.cs
+++ Assets/_Scripts/ChunkingSystem/LODManager.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections.Generic;
-
-
+using NUnit.Framework.Constraints;
 
 // What is the purpose of this class?
 // * To manage the number of vertices in each chunk as a function of sidelength.
 namespace _Scripts.ChunkingSystem
 {
+    
+    //ReSharper disable all
     public class LODManager
     {
+        // For the purposes of this program LOD represents the integer number of vertices used
+        // to represent the sidelength of a chunk
+        
         // This value represents the minimum size of a single chunk in integer multiples of
         // world units. (that is, the distance between (1,0,0) and (0,0,0) in unity. 
         
@@ -16,31 +20,48 @@ namespace _Scripts.ChunkingSystem
         // This variable needs sufficient input validation
         private int _minChunkSize = 120;
         private int[] _lods;
+        private int _maxLODLevel;
+
         public void SetMinChunkSize(int minChunkSize) { this._minChunkSize = minChunkSize; }
         public int GetMinChunkSize() {return this._minChunkSize;}
-        
-        public static int[] ComputeLODs(int number)
+        public int[] ComputeLODsFromMinChunkSize()
         {
             var factorList = new List<int>();
-
+            var number = _minChunkSize;
             if (number <= 0)
                 throw new ArgumentException("Number must be a positive integer.");
 
-            for (int i = 1; i <= Math.Sqrt(number); i++)
+            for (var i = 1; i <= Math.Sqrt(number); i++)
             {
-                if (number % i == 0)
-                {
-                    factorList.Add(i);
+                if (number % i != 0) continue;
+                factorList.Add(i);
 
-                    if (i != number / i) // Avoid adding square roots twice
-                        factorList.Add(number / i);
-                }
+                if (i != number / i) // Avoid adding square roots twice
+                    factorList.Add(number / i);
             }
 
             factorList.Sort(); // Optional: return factors in ascending order
 
-            int[] factorArray = factorList.ToArray();
+            var factorArray = factorList.ToArray();
+            this._lods = factorArray;
             return factorArray;
+        }
+
+        public void SetMaxLODLevel(int maxLODLevel)
+        {
+            _maxLODLevel = maxLODLevel;
+        }
+
+        public int GetMaxLODLevel()
+        {
+            return _maxLODLevel;
+        }
+
+        public int ComputeLOD(int level)
+        {
+            int totalNumberLODS = _lods.Length;
+            int lodIndex = Math.Min(_maxLODLevel, level);
+            return _lods[totalNumberLODS-lodIndex-1];
         }
     }
 }
